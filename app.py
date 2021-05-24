8b00a430978c1999dd9c8470347a4fafc2a58474
tajrian <tajrianbintajahid@gmail.com>
2021-05-24 14:17:16 +0600

    add post request in home method of app.py

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/',methods = ['GET','POST'])
 def home():
+
+    if request.method == "POST" :
+        userInfo = request.form
+        first_name = userInfo["first_name"]
+        last_name = userInfo["last_name"]
+        email = userInfo["email"]
+        mobile_number = userInfo["mobile_number"]
+
+        return "Hi" + first_name
     return render_template("home.html")
 
 if __name__ == "__main__":
