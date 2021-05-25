4702af792c515e15218603bf76da1bbd90993a5c
tajrian <tajrianbintajahid@gmail.com>
2021-05-25 23:54:03 +0600

    use flask-mysql to access mysql db

--- app.py
+++ app.py
@@ -1,6 +1,23 @@
 from flask import Flask, render_template, request
+from flaskext.mysql import MySQL
+import yaml 
 
 app = Flask(__name__)
+
+yaml_file = open("/home/user/dockerPractice/docTask/dockerTask/db.yaml", 'r')
+db = yaml.load(yaml_file)
+
+
+app.config['MYSQL_DATABASE_USER'] = db['mysql_user']
+app.config['MYSQL_DATABASE_PASSWORD'] = db['mysql_password']
+app.config['MYSQL_DATABASE_DB'] = db['mysql_db']
+app.config['MYSQL_DATABASE_HOST'] = db['mysql_host']
+ 
+
+mysql = MySQL()
+mysql.init_app(app)
+
+
 
 @app.route('/',methods = ['GET','POST'])
 def home():
@@ -11,6 +28,11 @@ def home():
         last_name = userInfo["last_name"]
         email = userInfo["email"]
         mobile_number = userInfo["mobile_number"]
+        conn = mysql.connect()
+        cursor =conn.cursor()
+        cursor.execute("INSERT INTO flask_user(first_name, last_name, email, phone_number) VALUES(%s, %s,%s, %s)",(first_name, last_name, email, mobile_number))
+        conn.commit()
+        return "done"
 
         return "Hi " + first_name
     return render_template("home.html")
