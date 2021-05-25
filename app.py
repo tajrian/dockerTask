2d4c06403d14a692655382de3c782f4ab9a62a05
tajrian <tajrianbintajahid@gmail.com>
2021-05-25 22:58:24 +0600

    connect mysql via flask-mysql

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
 from flask import Flask, render_template, request
+from flaskext.mysql import MySQL
 
 app = Flask(__name__)
+
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = ''
+app.config['MYSQL_DATABASE_DB'] = 'test'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+ 
+
+mysql = MySQL()
+mysql.init_app(app)
+
+
 
 @app.route('/',methods = ['GET','POST'])
 def home():
@@ -11,6 +23,11 @@ def home():
         last_name = userInfo["last_name"]
         email = userInfo["email"]
         mobile_number = userInfo["mobile_number"]
+        conn = mysql.connect()
+        cursor =conn.cursor()
+        cursor.execute("INSERT INTO flask_user(first_name, last_name, email, phone_number) VALUES(%s, %s,%s, %s)",(first_name, last_name, email, mobile_number))
+        conn.commit()
+        return "done"
 
         return "Hi " + first_name
     return render_template("home.html")
