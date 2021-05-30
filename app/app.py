be635df8a1285707e6a9a43ddd967a8f71cb5983
tajrian <tajrianbintajahid@gmail.com>
2021-05-31 02:35:38 +0600

    add soft delete logics

--- app/app.py
+++ app/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request
 from flaskext.mysql import MySQL
-import yaml 
+from flask import jsonify
 
 app = Flask(__name__)
 
@@ -10,11 +10,9 @@ app.config['MYSQL_DATABASE_USER'] = 'root'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
 app.config['MYSQL_DATABASE_DB'] = 'test'
 app.config['MYSQL_DATABASE_HOST'] = 'db'
- 
 
 mysql = MySQL()
 mysql.init_app(app)
-
 
 
 @app.route('/',methods = ['GET','POST'])
@@ -29,7 +27,7 @@ def home():
         active = 1
         conn = mysql.connect()
         cursor =conn.cursor()
-        cursor.execute("INSERT INTO appUser(first_name, last_name, email, phone_number, active) VALUES(%s, %s,%s, %s, %s)",(first_name, last_name, email, phone_number,active))
+        cursor.execute("INSERT INTO appUser(first_name, last_name, email, phone_number,active) VALUES(%s, %s,%s, %s,%s)",(first_name, last_name, email, phone_number,active))
         conn.commit()
         return render_template("editSuccess.html", message = "user created!")
         
@@ -37,7 +35,7 @@ def home():
 
     conn = mysql.connect()
     cursor =conn.cursor()
-    userValue = cursor.execute("select * from appUser")
+    userValue = cursor.execute("SELECT * FROM appUser WHERE active = 1")
     if userValue > 0:
         userDetails = cursor.fetchall()
         
@@ -49,24 +47,49 @@ def home():
 
 @app.route('/delete/<int:id>', methods=['get', 'post'])
 def delete(id):
-    
-    return ("Here to delete")
-    
+    conn = mysql.connect()
+    cursor =conn.cursor()
+    userValue = cursor.execute("UPDATE appUser SET active=0 WHERE user_id=%s",id)
+    conn.commit()
+    return render_template("editSuccess.html", message = "deleted!")
+
 
 
 @app.route('/edit/<int:id>', methods=['get', 'post'])
 def edit(id):
-    
-    return "Here to edit! "
+    conn = mysql.connect()
+    cursor =conn.cursor()
+    #print(type(id))
+    userValue = cursor.execute("SELECT user_id,first_name,last_name,email,phone_number FROM appUser WHERE user_id=%s",id)
+    if userValue > 0:
+        userDetails = cursor.fetchall()
+
+    if userDetails:
+        return render_template("edit.html", userDetails = userDetails ) 
+
+    return "We are lost dr kim! :("
     
 
 @app.route('/update',methods = ['GET','POST'])
 def update():
 
-    return ("Here to update!")
+    if request.method == "POST" :
+        userInfo = request.form
+        id = userInfo['id']
+        first_name = userInfo["first_name"]
+        last_name = userInfo["last_name"]
+        email = userInfo["email"]
+        phone_number = userInfo["phone_number"]
+        conn = mysql.connect()
+        cursor =conn.cursor()
+        cursor.execute ("""UPDATE appUser SET first_name=%s, last_name=%s, email=%s, phone_number=%s WHERE user_id=%s""", (first_name,last_name,email,phone_number,id))
+        conn.commit()
+    
+    return render_template("editSuccess.html", message = "edited !")
+
 
 
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True,host='0.0.0.0')
