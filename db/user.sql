7ec7646cedded2ef22c0d9020ab872cf155113ba
tajrian <tajrianbintajahid@gmail.com>
2021-05-31 11:57:35 +0600

    add demo users

--- db/user.sql
+++ db/user.sql
@@ -10,3 +10,7 @@ CREATE TABLE appUser (
   active int DEFAULT 1,
   PRIMARY KEY (`user_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+insert  into appUser(user_id,first_name,last_name,email,phone_number,active) values
+(1,'Tajrian Binta','Jahid','tajrian.jahid@newscred.com','01679350726',1),
+(2,'Demo','Person','demo.person@gmail.com','01679350726',1);
